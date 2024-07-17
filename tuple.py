26fd1fbcbfe76db81192acb0a490ccc20bb6ee51
Svetlan-37 <karassvetlana@yandex.ru>
2024-07-17 23:55:58 +0300

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,18 @@
+# кортежи
+name = 'Sveta'
+tuple_ = (1, 'a', 2, 'b', 3, 'c', name) * 2
+print(tuple_)
+tuple_ = [1, 2, 3], 'b', 'Olga', 5 > 7
+print(tuple_)
+tuple_ = [1, 2, 3], 'b', 'Olga', 5 > 7
+print(tuple_[2])
+# списки
+list_ = [[1, 2, 3], 'b', 'Olga', 5 > 7]
+print(list_)
+list_[0] = 'a'
+print(list_)
+print(list_[2])
+animal = ['cat', 'dog', 'mouse', 'bear']
+print(animal)
+animal.remove('bear')
+print(animal)
